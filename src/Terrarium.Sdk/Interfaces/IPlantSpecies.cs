bdcffa28d1e4588fa1170498c10261a8f2512f7b
Bil Simser <emailme@bilsimser.com>
2014-06-20 22:09:30 -0600

    Added missing interfaces

---
+++ src/Terrarium.Sdk/Interfaces/IPlantSpecies.cs
@@ -0,0 +1,27 @@
+﻿using Terrarium.Sdk.Enumerations;
+
+namespace Terrarium.Sdk.Interfaces
+{
+    /// <summary>
+    ///  <para>
+    ///   Species information properties that are applicable only to plants
+    ///   and that should be made available to developers.  Use this to find
+    ///   out general information about a particular plant species' capabilities.
+    ///  </para>
+    /// </summary>
+    public interface IPlantSpecies
+    {
+        /// <summary>
+        ///  <para>
+        ///   Returns the SkinFamily the creature will use when being displayed in the Terrarium.
+        ///  </para>
+        /// </summary>
+        /// <returns>
+        ///  System.String identifying the creature's skin family.
+        /// </returns>
+        PlantSkinFamily SkinFamily
+        {
+            get;
+        }         
+    }
+}
---
+++ src/Terrarium.Sdk/Interfaces/IPlantWorldBoundary.cs
@@ -0,0 +1,19 @@
+﻿using Terrarium.Sdk.Classes.State;
+
+namespace Terrarium.Sdk.Interfaces
+{
+    /// <summary>
+    ///  Represents a plant's view of the world.
+    /// </summary>
+    public interface IPlantWorldBoundary
+    {
+        /// <summary>
+        ///  Retrieves the most up to date plant state for the current
+        ///  plant.
+        /// </summary>
+        PlantState CurrentPlantState
+        {
+            get;
+        }         
+    }
+}
